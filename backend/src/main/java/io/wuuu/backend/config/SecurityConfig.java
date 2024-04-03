6a36668d8a003b2e65842eaf80439c9e0e3936dd
Max Demydenko <m_demy@pm.me>
2024-04-04 02:49:38 +0300

    fix: disable csrf

--- backend/src/main/java/io/wuuu/backend/config/SecurityConfig.java
+++ backend/src/main/java/io/wuuu/backend/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package io.wuuu.auth;
+package io.wuuu.backend.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -6,25 +6,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 @Configuration(proxyBeanMethods = false)
 @RequiredArgsConstructor
 public class SecurityConfig {
-
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize ->
                 authorize
                         .requestMatchers(
                                 "/graphql/**",
+                                "/graphiql/**",
                                 "/actuator/health"
                             ).permitAll()
                         .anyRequest().authenticated()
@@ -32,16 +26,7 @@ public class SecurityConfig {
 
         http.csrf(AbstractHttpConfigurer::disable);
 
-        return http.formLogin(withDefaults()).build();
+        return http.build();
     }
 
-    @Bean
-    public UserDetailsService users() {
-        UserDetails user = User.builder()
-                .username("admin")
-                .password("{noop}password")
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
-    }
 }
